8ad243abc23ca20ad0b30732a0845a947bb05bd8
Nick Pazoles <npazo@users.noreply.github.com>
2023-04-25 15:47:02 -0500

    change zsh install to --unattended

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -1,5 +1,5 @@
 # Install Oh My ZSH 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh --unattended)"
 
 # Install Powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
